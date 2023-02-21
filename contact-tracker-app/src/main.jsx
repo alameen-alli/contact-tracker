4f1c2cbc6e2404a71b2960dcd8bdf7776f283d02
Alameen8739 <allialameen8739@gmail.com>
2023-02-21 23:44:53 +0100

    root page

--- contact-tracker-app/src/main.jsx
+++ contact-tracker-app/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import App from './App'
+import './App.css'
+import Root from './routes/root';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- contact-tracker-app/src/routes/root.jsx
+++ contact-tracker-app/src/routes/root.jsx
@@ -1,9 +1,37 @@
-export default function Root () {
-    return (
-        <>
+export default function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>Contacts</h1>
         <div>
-            
+            <form id="" role="search">
+                <input
+                 type="search" 
+                 id="q"
+                 aria-aria-label="Search contacts"
+                 placeholder="Search"
+                 name="q"
+                />
+                <div id="search-spinner" aria-hidden hidden={true}>
+                    <div className="sr-only" aria-live="polite"></div>
+                </div>
+            </form>
+            <form method="form">
+                <button type="submit">New</button>
+            </form>
         </div>
-        </>
-    )
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+    </>
+  );
 }
