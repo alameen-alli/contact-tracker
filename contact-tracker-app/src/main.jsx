0a362798e07ae7cde5b1de1e4954a47b59d7c0a6
Alameen8739 <allialameen8739@gmail.com>
2023-02-22 22:03:29 +0100

    still routing

--- contact-tracker-app/src/main.jsx
+++ contact-tracker-app/src/main.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
 import './App.css'
 import Root from './routes/root';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>
-  }
-])
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
